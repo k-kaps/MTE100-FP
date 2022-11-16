21aabe1e5373f37113789319c3c0cd47b1fa4918
Karan Kapoor <108018084+k-kaps@users.noreply.github.com>
2022-11-16 16:24:05 -0500

    Update FinalProject-v1.0.c

--- FinalProject-v1.0.c
+++ FinalProject-v1.0.c
@@ -5,39 +5,88 @@ Motor to Send Cards to Phase 2: motorB
 
 In Phase 2:
 Motor to Regulate Scanning of Cards: motorC
+
+In Phase 3: 
+Motor to flip the cards: motorD
 */
-void cardScanningP2()
+
+bool touchWait(inty waittimeMS)
+{
+	/*
+	Description: To make the touch sensor wait for specific times for an input
+	Parameters:
+	int waittimeMS: stores the milliseconds to wait for an input
+	Return:
+	true/false depending on the pressing of the touch sensor
+	*/
+	time1[T1] = 0;
+	while (T1 <= waittimeMS)
+	{
+		if(SensorValue[S1] == 1)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+int cardScanning()
+{
+	/*
+	Description: To calculate the value of the card
+	Parameters:
+	None
+	Return:
+	int cardvalue: stores the value of the card
+	*/
+	int cardvalue = 0;
+	if (SensorValue[S1] == int(colorBlack))
+	{
+		cardvalue = 1;
+	}
+	else if (SensorValue[S1] == int(colorBlue))
+	{
+		cardvalue = 2;
+	}
+	else if (SensorValue[S1] == int(colorGreen))
+	{
+		cardvalue = 3;
+	}
+	else if (SensorValue[S1] == int(colorYellow))
+	{
+		cardvalue = 4;
+	}
+	else if (SensorValue[S1] == int(colorRed))
+	{
+		cardvalue = 5;
+	}
+	else if (SensorValue[S1] == int(colorBrown))
+	{
+		cardvalue = 6;
+	}
+	else
+	{
+		cardvalue = -1;
+	}
+	return cardvalue;
+}
+void cardDistribution_P2()
 {
 
 }
-void sendCardP1(int timedelay_1, int timedelay_2)
+void cardDistribution_P1(int timedelay)
 {
-      motor[motorA] = 10;
-      wait1Msec(timedelay_1);
-      motor[motorA] = 0;
-      motor[motorB] = -90;
-      motor[motorA] = -60;
-      wait1Msec(timedelay_1);
-      motor[motorA] = 0;
-      motor[motorA] = 50;
-      wait1Msec(timedelay_2);
-      motor[motorB] = 0;
-      motor[motorA] = 0;
+	//Set MotorB on HIGH SPEED
+	motor[motorB] = -70;
+	//Set MotorA on LOW SPEED
+	motor[motorA] = -50;
+	wait1Msec(timedelay);
+	motor[motorA] = 0;
+	motor[motorA] = 30;
+	wait1Msec(timedelay);
+	motor[motorB] = 0;
+	motor[motorA] = 0;
 }
-//void configureAllSensors(){}
+//void configureAllSensors(){ // MEVAN}
 task main()
 {
-      for(int i=1; i<=3;i++)
-      {
-            for(int j=1; j<=3; j++)
-            {
-                  motor[motorC] = -5
-                  sendCardP1(370,650);
-                  wait1Msec(200);
-            }
-                  motor[motorA]=30;
-                  motor[motorB] = 30;
-                  int timedelay_3 = 600;
-                  wait1Msec(timedelay_3);
-      }
 }
